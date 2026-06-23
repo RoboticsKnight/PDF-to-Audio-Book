6ff0a1f7fed7e015ccc93ecc12baf95351ef6b62
RoboticsKnight <toluwanimiomoyeni30@gmail.com>
2026-06-23 16:11:14 +0200

    Can now access the PDF

--- pdf_to_audio_book.py
+++ pdf_to_audio_book.py
@@ -1,5 +1,20 @@
 import pyttsx3
+import PyPDF2
 
+
+# =====================================================================
+# Accessing PDF
+# =====================================================================
+book = open('OOP.pdf', 'rb')
+pdfReader = PyPDF2.PdfReader(book)
+
+# Getting the number of pages in the PDF
+pages = len(pdfReader.pages)
+print(pages)
+
+# =====================================================================
+# Text to speach
+# =====================================================================
 speaker = pyttsx3.init()
 speaker.say('Look child I can talk')
 speaker.runAndWait()
